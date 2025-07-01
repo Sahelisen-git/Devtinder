49101abd1a2509b93b2087ea92312197f93838e1
Sahelisen-git <sensaheli1234@gmail.com>
2025-07-01 21:57:31 +0530

    middleware explore

--- src/app.js
+++ src/app.js
@@ -1,22 +1,25 @@
 const express=require('express');
 const app=express();
 
-app.get("/user",(req,res)=> {
-    res.send({firstnmae:"SAHELI",lastname:"Sen"})
-});
-app.post("/user",(req,res)=>{
-    //data succcessfully save to DB
-    res.send("data succcessfully save to DB");
-});
- app.delete("/user",(req,res)=>{
-    res.send("deleted succeessfully");
- });
- app.use("/hello",(req,res) =>{
- res.send(" moon  saheli from server");
- });
-// app.use("/",(req,res) =>{
-//  res.send(" hello hello");
-//  });
+
+
+app.use("/user",
+    (req,res,next) =>{
+// res.send("route 1");
+ 
+ next();
+  console.log("server is succesfully route1");
+ },
+ (req,res,next) =>{
+// res.send("route 2");
+  console.log("server is succesfully route2");
+ next();
+ },
+ (req,res) =>{
+ res.send("route 3");
+  console.log("server is succesfully route3");
+ }
+);
 
 app.listen(3000, ()=>{
     console.log("server is succesfully running on port 3000");
