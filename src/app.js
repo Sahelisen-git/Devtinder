1e991c01b9d7cbc560832aa4e1a411feb77cb632
Sahelisen-git <sensaheli1234@gmail.com>
2025-07-02 02:12:51 +0530

    Wild-card-errorhandling

--- src/app.js
+++ src/app.js
@@ -1,12 +1,14 @@
 const express=require('express');
 const app=express();
 
-const { adminAuth }=require("./middlewares/Auth")
-
-app.use("/admin", adminAuth
-    );
-app.get("/admin/userAllaAta",(req,res)=>{
+app.get("/userAllaAta",(req,res)=>{
+    throw new Error("gygjhg");
 res.send("successfull");
+});
+app.use("/",(err,req,res,next)=>{
+    if(err){
+        res.status(500).send("Something went Wrong");
+    }
 });
 app.listen(3000, ()=>{
     console.log("server is succesfully running on port 3000");
