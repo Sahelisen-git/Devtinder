13a5490ff8d3d695d5f9f547ccaa9fa268ad84f2
Sahelisen-git <sensaheli1234@gmail.com>
2025-11-16 13:43:32 +0530

    Build feed API

--- src/routes/user.js
+++ src/routes/user.js
@@ -3,6 +3,7 @@ const { contains } = require('validator');
 const userRouter = express.Router();
  const {userAuth}=require("../middlewares/Auth");
  const connectionRequest= require("../models/connectionRequest");
+    const User = require("../models/user");
 
 //get all the pending connetion requests for the logged in user
 userRouter.get("/user/requests/received" ,userAuth,async(req,res)=>{
@@ -53,5 +54,46 @@ try{
 } catch(err){
     res.status(400).send("Error"+err.message);
 }
+});
+
+// User should see all the user cards except
+// his own card
+//his connections
+//ignored  users
+//already sent connection requests
+userRouter.get("/feed", userAuth, async (req,res)=> {
+    try{
+
+        const loggedInUser= req.user;
+         const page= parseInt(req.query.page) || 1;
+         let limit= parseInt(req.query.limit) || 10;
+         limit= limit > 50 ? 50 : limit; //max limit is 50
+         const skip= (page -1) * limit;
+        // find all connections  requests(sent+recevied) of the logged in user
+        const connectionRequests= await connectionRequest.find({
+            $or:[
+                { toUserId: loggedInUser._id },
+                { fromUserId: loggedInUser._id}
+            ],
+        })
+        .select("fromUserId toUserId");
+
+        const hideUsersFeed= new Set();
+             connectionRequests.forEach((req) => {
+            hideUsersFeed.add(req.fromUserId.toString());
+            hideUsersFeed.add(req.toUserId.toString());
+        });
+
+         //fetching only fromUserId and toUserId fields
+         const users= await User.find({
+            $and:[
+                { _id: { $ne: loggedInUser._id } }, //excluding his own card
+                { _id: { $nin: Array.from(hideUsersFeed) } }, //excluding his connections and ignored users and already sent connection requests
+            ],
+         }).select("firstName lastName photoUrl Skills About").skip(skip).limit(limit); //fetching only required fields
+         res.send(users);
+    }catch(err){
+        res.status(400).send("Error"+err.message);
+    }
 });
 module.exports= userRouter;
